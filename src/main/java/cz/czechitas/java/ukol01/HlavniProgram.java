b1d962a4e3b12d11012ad62d36b98d2c9ab04a6e
vlkova <rrozte@gmail.com>
2025-04-09 09:06:20 +0200

    Cast 1 / nakresli prasatko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -11,9 +11,59 @@ public class HlavniProgram {
 
     public void start() {
         zofka = new Turtle();
-
         nakresliPrasatko();
 
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnRight(90);
+
+        nakresliMnohouhlenik(50, 45);
+
+        zofka.turnRight(90);
+        zofka.move(120);
+        zofka.turnLeft(90);
+        zofka.move(25);
+
+        nakresliMnohouhlenik(22.5, 20);
+        //to je jakoby kolecko
+
+        zofka.turnRight(90);
+        zofka.move(220);
+        zofka.turnLeft(90);
+
+        nakresliSlunicko(30);
+
+    }
+
+    public void nakresliSlunicko(double delkaStrany) {
+        //dvanactiuhelnik - uhel mezi stranami 30
+        for (int i = 0; i <= 12 - 1; i++) {
+
+            zofka.penDown();
+            zofka.move(delkaStrany / 2);
+            zofka.turnLeft(75);
+            zofka.move(delkaStrany);
+            zofka.turnLeft(75);
+            zofka.penUp();
+            zofka.move(delkaStrany / 2);
+            zofka.turnRight(180);
+
+        }
+        zofka.penUp();
+    }
+
+
+    public void nakresliMnohouhlenik(double delkaStrany, double uhel) {
+        zofka.turnLeft(uhel);
+        for (int i = 0; i <= 360 / uhel - 1; i++) {
+
+            zofka.penDown();
+            zofka.move(delkaStrany);
+            zofka.turnRight(uhel);
+        }
+        zofka.penUp();
+        zofka.turnRight(uhel);
     }
 
     public void nakresliPrasatko() {
